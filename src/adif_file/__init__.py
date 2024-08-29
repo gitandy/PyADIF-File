267d1562803c6f2ad326c903fea28c0ef54b755f
Andreas Schawo <andreas@schawo.de>
2024-08-29 07:39:18 +0200

    Fixed license, copyright and pyproject.toml

--- src/adif_file/__init__.py
+++ src/adif_file/__init__.py
@@ -4,7 +4,7 @@ from adif_file.__version__ import __version__ as __version_str__
 
 __author_name__ = 'Andreas Schawo, DF1ASC'
 __author_email__ = 'andreas@schawo.de'
-__copyright__ = 'Copyright 2023 by Andreas Schawo,licensed under CC BY-SA 4.0'
+__copyright__ = 'Copyright 2023-2024 by Andreas Schawo,licensed under CC BY-SA 4.0'
 __proj_name__ = 'PyADIF-File'
 __version__ = __version_str__[1:].split('-')[0]
 __description__ = 'Convert ADIF ADI/ADX content to dictionary and vice versa'
