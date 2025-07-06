0d1a9a89eca552e0d3ccc97e4e50b8b09f23401b
Andreas Schawo <andreas@schawo.de>
2025-07-06 13:52:49 +0200

    Added regex format checks to util

--- src/adif_file/util.py
+++ src/adif_file/util.py
@@ -2,7 +2,7 @@
 # To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
 
 """Provides some useful function to handle ADIF data"""
-
+import re
 import sys
 import datetime
 
@@ -17,6 +17,7 @@ def get_cur_adif_dt() -> str:
 
 def adif_date2iso(date: str) -> str:
     """Tries to convert ADIF date to iso format"""
+    # Todo: should raise a ValueError if wrong format
     if not date or len(date) != 8:
         return date
     return date[:4] + '-' + date[4:6] + '-' + date[6:8]
@@ -24,9 +25,81 @@ def adif_date2iso(date: str) -> str:
 
 def adif_time2iso(time: str) -> str:
     """Tries to convert ADIF time to iso format"""
+    # Todo: should raise a ValueError if wrong format
     if not time or len(time) not in (4, 6):
         return time
     return time[:2] + ':' + time[2:4] + (':' + time[4:6] if len(time) == 6 else '')
 
 
-__all__ = ['get_cur_adif_dt', 'adif_date2iso', 'adif_time2iso']
+def iso_date2adif(date: str) -> str:
+    """Converts an ISO formated date to ADIF"""
+    if not check_format(REGEX_ISODATE, date):
+        raise ValueError(f'Not a valide ISO date "{date}"')
+    return date.replace('-', '')
+
+
+def iso_time2adif(time: str) -> str:
+    """Converts an ISO formated time to ADIF"""
+    if not check_format(REGEX_ISOTIME, time):
+        raise ValueError(f'Not a valide ISO time "{time}"')
+    return time.replace(':', '')
+
+
+REGEX_CALL = re.compile(r'([a-zA-Z0-9]{1,3}?/)?([a-zA-Z0-9]{1,3}?[0-9][a-zA-Z0-9]{0,3}?[a-zA-Z])(/[aAmMpPrRtT]{1,2}?)?')
+REGEX_RST = re.compile(r'([1-5]([1-9]([1-9][aAcCkKmMsSxX]?)?)?)|([-+][0-9]{1,2})')
+REGEX_LOCATOR = re.compile(r'[a-rA-R]{2}[0-9]{2}([a-xA-X]{2}([0-9]{2})?)?')
+REGEX_NONASCII = re.compile(r'[ -~\n\r]*(.)?')
+REGEX_ADIFTIME = re.compile(r'(([0-1][0-9])|(2[0-3]))([0-5][0-9])([0-5][0-9])?')
+REGEX_ADIFDATE = re.compile(r'([1-9][0-9]{3})((0[1-9])|(1[0-2]))((0[1-9])|([1-2][0-9])|(3[0-1]))')
+REGEX_ISOTIME = re.compile(r'(([0-1][0-9])|(2[0-3])):([0-5][0-9])(:[0-5][0-9])?')
+REGEX_ISODATE = re.compile(r'([1-9][0-9]{3})-((0[1-9])|(1[0-2]))-((0[1-9])|([1-2][0-9])|(3[0-1]))')
+# noinspection RegExpRedundantEscape
+REGEX_EMAIL = re.compile(r'[\w\-\.]+@([\w-]+\.)+[\w-]{2,}')
+
+
+def check_format(exp: re.Pattern, txt: str) -> bool:
+    """Test the given text against a regular expression
+    :param exp: a compiled pattern (e.g. util.REGEX_*)
+    :param txt: a text
+    :return: true if pattern matches"""
+    return bool(re.fullmatch(exp, txt))
+
+
+def check_call(call: str) -> None | tuple:
+    """Test a call sign against a regular expression
+    :param call: a call sign
+    :return: tuple of parts ('Country prefix/', 'Call sign', '/Operation suffix')"""
+    m = re.fullmatch(REGEX_CALL, call)
+    if m:
+        return m.groups()
+    return None
+
+
+def find_non_ascii(text: str) -> set:
+    """Find all non ASCII chars in a text
+    :param text: the text to search in
+    :return: a set of non ASCII chars"""
+    non_ascii = []
+    for c in re.findall(REGEX_NONASCII, text):
+        if c:
+            non_ascii.append(c)
+    return set(non_ascii)
+
+
+def replace_non_ascii(text: str, replace: dict[str, str] = None, default: str = '_') -> str:
+    """Replaces every non ASCII char with a str from a mapping
+    :param text: the text containing non ASCII chars
+    :param replace: a mapping with non ASCII chars and suiting substitutes (e.g. {'ä':'ae', 'ß':'ss'})
+    :param default: the default substitute if no mapping is found
+    :return: text with substitutes"""
+    replace = replace if type(replace) is dict else {}
+    default = default if type(default) is str else '_'
+    for na in find_non_ascii(text):
+        text = text.replace(na, replace.get(na, default))
+    return text
+
+
+__all__ = ['get_cur_adif_dt', 'adif_date2iso', 'adif_time2iso', 'iso_date2adif', 'iso_time2adif',
+           'check_format', 'check_call', 'replace_non_ascii',
+           'REGEX_ADIFDATE', 'REGEX_ADIFTIME', 'REGEX_ISODATE', 'REGEX_ISOTIME',
+           'REGEX_EMAIL', 'REGEX_RST', 'REGEX_LOCATOR', 'REGEX_CALL']
---
+++ test/test_util.py
@@ -0,0 +1,113 @@
+# PyADIF-File (c) 2025 by Andreas Schawo is licensed under CC BY-SA 4.0.
+# To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+
+import unittest
+
+from adif_file.util import *
+
+
+class Util(unittest.TestCase):
+    def test_10_conv_adfidate(self):
+        self.assertEqual('2025-04-25', adif_date2iso('20250425'))
+        self.assertEqual('2025042', adif_date2iso('2025042'))  # Todo: should raise an ValueError
+
+    def test_20_conv_adfitime(self):
+        self.assertEqual('11:25', adif_time2iso('1125'))
+        self.assertEqual('18:33:45', adif_time2iso('183345'))
+        self.assertEqual('18334', adif_time2iso('18334'))  # Todo: should raise an ValueError
+
+    def test_30_conv_isodate(self):
+        self.assertEqual('20250425', iso_date2adif('2025-04-25'))
+        self.assertRaises(ValueError, iso_date2adif, '2025-042')
+
+    def test_40_conv_isotime(self):
+        self.assertEqual('1125', iso_time2adif('11:25'))
+        self.assertEqual('183345', iso_time2adif('18:33:45'))
+        self.assertRaises(ValueError, iso_time2adif, '20:2542')
+        self.assertRaises(ValueError, iso_time2adif, '7:25')
+
+    def test_100_call_format(self):
+        self.assertTrue(check_format(REGEX_CALL, 'Df1ASC'))
+        self.assertTrue(check_format(REGEX_CALL, 'DF1ASc/P'))
+        self.assertTrue(check_format(REGEX_CALL, 'HB9/dF1ASC'))
+        self.assertFalse(check_format(REGEX_CALL, 'HB9/DF1ASC/MOBILE'))
+        self.assertFalse(check_format(REGEX_CALL, 'DF1 ASC'))
+        self.assertTrue(check_format(REGEX_CALL, '2N2XCV'))
+        self.assertTrue(check_format(REGEX_CALL, 'N2X'))
+
+    def test_105_check_call(self):
+        self.assertTupleEqual((None, 'DF1ASC', None), check_call('DF1ASC'))
+        self.assertTupleEqual((None, 'DF1ASC', '/m'), check_call('DF1ASC/m'))
+        self.assertTupleEqual(('HB9/', 'DF1ASC', None), check_call('HB9/DF1ASC'))
+        self.assertTupleEqual(('HB9/', 'DF1ASC', '/MM'), check_call('HB9/DF1ASC/MM'))
+        self.assertIsNone(check_call('HB9/DF1ASC/MOBILE'))
+        self.assertIsNone(check_call('DF1 ASC'))
+
+    def test_110_adifdate_format(self):
+        self.assertTrue(check_format(REGEX_ADIFDATE, '20251231'))
+        self.assertFalse(check_format(REGEX_ADIFDATE, '2025-12-31'))
+        self.assertFalse(check_format(REGEX_ADIFDATE, '251231'))
+        self.assertFalse(check_format(REGEX_ADIFDATE, '20251331'))
+        self.assertFalse(check_format(REGEX_ADIFDATE, '20250132'))
+
+    def test_120_adiftime_format(self):
+        self.assertTrue(check_format(REGEX_ADIFTIME, '2359'))
+        self.assertTrue(check_format(REGEX_ADIFTIME, '235959'))
+        self.assertFalse(check_format(REGEX_ADIFTIME, '617'))
+        self.assertFalse(check_format(REGEX_ADIFTIME, '06:17'))
+        self.assertFalse(check_format(REGEX_ADIFTIME, '6:17'))
+        self.assertFalse(check_format(REGEX_ADIFTIME, '1260'))
+
+    def test_130_isodate_format(self):
+        self.assertTrue(check_format(REGEX_ISODATE, '2025-12-31'))
+        self.assertFalse(check_format(REGEX_ISODATE, '20251231'))
+        self.assertFalse(check_format(REGEX_ISODATE, '25-12-31'))
+        self.assertFalse(check_format(REGEX_ISODATE, '2025-13-31'))
+        self.assertFalse(check_format(REGEX_ISODATE, '2025-01-32'))
+
+    def test_140_isotime_format(self):
+        self.assertTrue(check_format(REGEX_ISOTIME, '23:59'))
+        self.assertTrue(check_format(REGEX_ISOTIME, '23:59:59'))
+        self.assertFalse(check_format(REGEX_ISOTIME, '6:17'))
+        self.assertFalse(check_format(REGEX_ISOTIME, '0617'))
+        self.assertFalse(check_format(REGEX_ISOTIME, '617'))
+        self.assertFalse(check_format(REGEX_ISOTIME, '12:60'))
+
+    def test_150_locator_format(self):
+        self.assertTrue(check_format(REGEX_LOCATOR, 'aa11'))
+        self.assertTrue(check_format(REGEX_LOCATOR, 'aa11gg'))
+        self.assertTrue(check_format(REGEX_LOCATOR, 'aa11gg99'))
+        self.assertTrue(check_format(REGEX_LOCATOR, 'aa00ff'))
+        self.assertTrue(check_format(REGEX_LOCATOR, 'RR99xx'))
+        self.assertFalse(check_format(REGEX_LOCATOR, 'aa99zz'))
+        self.assertFalse(check_format(REGEX_LOCATOR, 'zz99aa'))
+        self.assertFalse(check_format(REGEX_LOCATOR, 'aa54e'))
+        self.assertFalse(check_format(REGEX_LOCATOR, 'aa5ee'))
+
+    def test_160_rst_format(self):
+        self.assertTrue(check_format(REGEX_RST, '59'))
+        self.assertTrue(check_format(REGEX_RST, '599'))
+        self.assertTrue(check_format(REGEX_RST, '597m'))
+        self.assertTrue(check_format(REGEX_RST, '111A'))
+        self.assertFalse(check_format(REGEX_RST, '11A'))
+        self.assertFalse(check_format(REGEX_RST, '01'))
+        self.assertFalse(check_format(REGEX_RST, '10'))
+
+        # Digi modes
+        self.assertTrue(check_format(REGEX_RST, '12'))
+        self.assertTrue(check_format(REGEX_RST, '+12'))
+        self.assertTrue(check_format(REGEX_RST, '+6'))
+        self.assertTrue(check_format(REGEX_RST, '-5'))
+        self.assertTrue(check_format(REGEX_RST, '-35'))
+        self.assertFalse(check_format(REGEX_RST, '+100'))
+        self.assertFalse(check_format(REGEX_RST, '-223'))
+
+    def test_200_replace_nonascii(self):
+        txt_non_ascii = 'Es saß ´ne hübsche YL am Funkegrät und funkte, nach Östereich auf 70cm sie blödelte und unkte'
+        txt_ascii_default = 'Es sa_ _ne h_bsche YL am Funkegr_t und funkte, nach _stereich auf 70cm sie bl_delte und unkte'
+        txt_ascii_defsharp = 'Es sa# #ne h#bsche YL am Funkegr#t und funkte, nach #stereich auf 70cm sie bl#delte und unkte'
+        txt_ascii_de = 'Es sass \'ne huebsche YL am Funkegraet und funkte, nach Oestereich auf 70cm sie bloedelte und unkte'
+        ascii_map = {'ß':'ss', '´':'\'', 'ä': 'ae', 'ö': 'oe', 'Ö': 'Oe', 'ü': 'ue'}
+        self.assertEqual(txt_ascii_default, replace_non_ascii(txt_non_ascii))
+        self.assertEqual(txt_ascii_defsharp, replace_non_ascii(txt_non_ascii, default='#'))
+        self.assertEqual(txt_ascii_de, replace_non_ascii(txt_non_ascii, ascii_map))
